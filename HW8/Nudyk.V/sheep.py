3e3a612eb5c785ff4f4d5ee9e76aa7c08ab16fe9
Volodia Nudyk <volodianudyk@gmail.com>
2022-10-07 21:38:44 +0300

    Added HW8 -> sheep.py

---
+++ HW8/Nudyk.V/sheep.py
@@ -0,0 +1,8 @@
+# Consider an array/list of sheep where some sheep may be missing from their place. 
+# We need a function that counts the number of sheep present in the array (true means present).
+
+def count_sheeps(sheep):
+    return sheep.count(True)
+    
+
+print(count_sheeps([True, False, 1, None, 10>9, 0]))
