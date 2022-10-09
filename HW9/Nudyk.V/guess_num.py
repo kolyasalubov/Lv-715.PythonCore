2bfeccd7084d899e8468609305d52f0d716e264e
Volodia Nudyk <volodianudyk@gmail.com>
2022-10-09 14:27:55 +0300

    Added HW9 -> guess_num.py

---
+++ HW9/Nudyk.V/guess_num.py
@@ -0,0 +1,20 @@
+from random import randint
+
+def guess_num():
+    ''' Executes a game of guessing the num between 1 and 10000
+    '''
+
+    random_num = randint(9999, 10000)
+    user_num = int(input('Guess the frickin number form 1 to 10000: '))
+    while user_num != random_num:
+        if user_num > random_num:
+            user_num = int(input('No, the num is lower. Try again: '))
+        if user_num < random_num:
+            user_num = int(input('No, the num is higher. Try again: '))
+    
+    print('You got the right num:', random_num, '!')
+
+
+guess_num()
+
+
