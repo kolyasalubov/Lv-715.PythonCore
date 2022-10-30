449569186aca201a68831c6fe087638a4d9fb236
Sabadash Yurii <yurasabadash54@gmail.com>
2022-10-30 22:41:06 +0200

    Added first task of HW3

--- HW3/YSaba/Task1.py
+++ HW3/YSaba/Task1.py
@@ -1,10 +1,9 @@
-HW2 , task 1
-
-#task1.2
-The_Zen_of_Python = "Beautiful is better than ugly. Explicit is better than implicit.Simple is better than complex.Errors should never pass silently.Now is better than never."
-print(The_Zen_of_Python.upper())
-
-#task1.3
-The_Zen_of_Python = "Beautiful is better than ugly. Explicit is better than implicit.Simple is better than complex.Errors should never pass silently.Now is better than never."
-result = The_Zen_of_Python.replace("i","&")
-print(result)
+#HW2 , task 1
+philosophy_str = "Beautiful is better than ugly. Explicit is better than implicit.Simple is better than complex.Errors should never pass silently.Now is better than never ."
+philosophy_str1 = philosophy_str.split()
+n1 = philosophy_str1.count("better")
+n2 = philosophy_str1.count("never")
+n3 = philosophy_str1.count("is")
+print(f"There are {n1} \"better\" , {2} \"never\" and {3} \"is\" in this string . \n")
+print("Display all text in upper case :\n",philosophy_str.upper())
+print("Replace all occurrences of the symbol “and” with “&” :\n", philosophy_str.replace("i" , "&"))
--- HW3/YSaba/Task2.py
+++ HW3/YSaba/Task2.py
@@ -1,5 +1,13 @@
-HW2, task 2.
+#hm
 
+philosophy_str = "Beautiful is better than ugly. Explicit is better than implicit.Simple is better than complex.Errors should never pass silently.Now is better than never ."
+philosophy_str1 = philosophy_str.split()
+n1 = philosophy_str1.count("better")
+n2 = philosophy_str1.count("never")
+n3 = philosophy_str1.count("is")
+print(f"There are {n1} \"better\" , {2} \"never\" and {3} \"is\" in this string .")
+print("Display all text in upper case :\n",philosophy_str.upper())
+print("Replace all occurrences of the symbol “and” with “&” :\n", philosophy_str.replace("i" , "&"))
 #task 2.1
 number = int(input("Enter a 4-digit number: "))
 n1 = number % 10
